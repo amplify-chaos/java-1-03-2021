5f4f3d5e514a3134c2c8f93412e5fd35c31dff21
amplify-chaos <imago.glass@gmail.com>
2021-05-16 20:06:15 +0400

    Homework 1 lvl 2 done

---
+++ src/main/java/Lesson1lvl2/JumpableRunnable.java
@@ -0,0 +1,9 @@
+package Lesson1lvl2;
+
+public interface JumpableRunnable {
+    int getMaxHeight();
+    void jump();
+
+    int getMaxLength();
+    void run();
+}
---
+++ src/main/java/Lesson1lvl2/Obstaclable.java
@@ -0,0 +1,6 @@
+package Lesson1lvl2;
+
+public interface Obstaclable {
+    boolean toRun(int maxLength);
+    boolean toJump(int maxHeight);
+}
