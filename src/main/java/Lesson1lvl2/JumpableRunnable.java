96589450ac856c4589d4ef7efb44f97aef9d6616
amplify-chaos <81295016+amplify-chaos@users.noreply.github.com>
2021-05-17 19:09:57 +0400

    Delete src/main/java/Lesson1lvl2 directory

--- src/main/java/Lesson1lvl2/JumpableRunnable.java
+++
@@ -1,9 +0,0 @@
-package Lesson1lvl2;
-
-public interface JumpableRunnable {
-    int getMaxHeight();
-    void jump();
-
-    int getMaxLength();
-    void run();
-}
--- src/main/java/Lesson1lvl2/Obstaclable.java
+++
@@ -1,6 +0,0 @@
-package Lesson1lvl2;
-
-public interface Obstaclable {
-    boolean toRun(int maxLength);
-    boolean toJump(int maxHeight);
-}
