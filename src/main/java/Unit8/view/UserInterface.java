8c395386d271cddf974b204fbd1c8de1915f477a
amplify-chaos <imago.glass@gmail.com>
2021-06-06 13:24:20 +0400

    ДЗ 8

---
+++ src/main/java/Unit8/view/UserInterface.java
@@ -0,0 +1,70 @@
+package Unit8.view;
+
+import Unit8.controller.Controller;
+import Unit8.controller.IController;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.Scanner;
+
+public class UserInterface implements IUserInterface {
+
+    IController controller = new Controller();
+    @Override
+    public void showMenu() {
+        while (true) {
+            System.out.println("\nПрограмма начала работу.");
+            System.out.println("Введите 1 или 2 для получения желаемых данных:");
+            System.out.println("\n1 - хочу знать о погоде в каком-то городе\n2 - хочу получить информацию из предыдущих обращений к программе\n \nвведите 'exit' для выхода");
+
+            Scanner scanner = new Scanner(System.in);
+
+            String menuChoice = scanner.nextLine();
+
+            checkIsExit(menuChoice);
+
+
+            if (menuChoice.equalsIgnoreCase("2")) {
+                try {
+                    controller.onCommandChosen(3);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                System.out.println("Введите имя города на латинице:");
+                String userResponse = scanner.nextLine();
+
+                try {
+                    controller.onCityInput(userResponse);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    continue;
+                }
+
+                System.out.println("Введите команду\n1 - для получения погоды на текущий день\n2 - для получения погоды на 5 дней");
+
+
+                System.out.println("* * * * * * * * * *");
+                int selectedCommand = scanner.nextInt();
+
+                try {
+                    controller.onCommandChosen(selectedCommand);
+                } catch (IOException | ParseException e) {
+                    e.printStackTrace();
+                    continue;
+                }
+            }
+        }
+    }
+
+    private void checkIsExit(String userResponse) {
+        if (userResponse.equalsIgnoreCase("exit") ||
+                userResponse.equalsIgnoreCase("выход")) {
+
+            System.out.println("Программа завершает работу. Хорошего дня!");
+            System.exit(0);
+        }
+    }
+}
