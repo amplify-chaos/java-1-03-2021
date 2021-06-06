58619c7064827bb9736c1d588465904c1ea91842
amplify-chaos <imago.glass@gmail.com>
2021-06-06 09:58:01 +0400

    ДЗ 7

---
+++ src/main/java/Unit7vol2/Main.java
@@ -0,0 +1,13 @@
+package Unit7vol2;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        UserInterface userInterface = new UserInterface();
+        userInterface.runTerminal();
+
+    }
+
+}
